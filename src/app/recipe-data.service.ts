841ac54836a9610f68d7e102faba1a542a259659
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-07-26 16:19:24 +0530

    Service

---
+++ src/app/recipe-data.service.ts
@@ -0,0 +1,23 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+export class RecipeDataService {
+
+
+  recipeData = [
+    {
+      itemTitle : "Chicken Biriyani",
+      itemposter :"https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/4:3/w_5100,h_3825,c_limit/RoastChicken_RECIPE_080420_37993.jpg",
+      itemImage : "https://m.media-amazon.com/images/I/81j-Ml3dQfL._AC_UF1000,1000_QL80_.jpg",
+      itemType : "Non-Veg",
+      itemDuration : "55 Minnutes",
+      itemLink : "",
+      itemProcedure : "",
+      itemIngredients : "",
+    }
+  ]
+
+  constructor() { }
+}
