49fafa9f927288848169b3262dfff85e2e2dda8d
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-07-26 16:22:44 +0530

    recipeData

--- src/app/recipe-data.service.ts
+++ src/app/recipe-data.service.ts
@@ -13,9 +13,9 @@ export class RecipeDataService {
       itemImage : "https://m.media-amazon.com/images/I/81j-Ml3dQfL._AC_UF1000,1000_QL80_.jpg",
       itemType : "Non-Veg",
       itemDuration : "55 Minnutes",
-      itemLink : "",
-      itemProcedure : "",
-      itemIngredients : "",
+      itemLink : "https://www.youtube.com/watch?v=95BCU1n268w&ab_channel=SpiceEats",
+      itemProcedure :  "Marinate chicken with yogurt, ginger, garlic, and spices. Cook with ground masala and rice.",
+      itemIngredients : "Chicken, basmati rice, yogurt, ginger, garlic, onions, ghee, cooking oil, spices",
     }
   ]
 
