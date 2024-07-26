1aa1641965000a9dd861338cba8cc2b9cab2cdf4
Nithin1297 <nithin.madduri@proclink.com>
2024-07-26 15:50:38 +0530

    started

---
+++ src/app/recipecard/recipecard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipecard',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipecard.component.html',
+  styleUrl: './recipecard.component.scss'
+})
+export class RecipecardComponent {
+
+}
