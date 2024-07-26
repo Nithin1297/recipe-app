e0a5121b2264f4dec1164cf0539e4102dc636989
Nithin1297 <nithin.madduri@proclink.com>
2024-07-26 15:58:44 +0530

    reciepte added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipecardComponent } from './recipecard/recipecard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RecipecardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/recipecard/recipecard.component.ts
+++ src/app/recipecard/recipecard.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { AppComponent } from '../app.component';
+
 
 @Component({
   selector: 'app-recipecard',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule,AppComponent],
   templateUrl: './recipecard.component.html',
   styleUrl: './recipecard.component.scss'
 })
