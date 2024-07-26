402ec21d9346585bd531155cd1e38dbcdc4c6031
Nithin1297 <nithin.madduri@proclink.com>
2024-07-26 16:17:25 +0530

    bdfb

--- src/app/recipecard/recipecard.component.ts
+++ src/app/recipecard/recipecard.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from '../app.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 
 @Component({
   selector: 'app-recipecard',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,AppComponent],
+  imports: [MatCardModule, MatButtonModule,AppComponent,MatIconModule,MatBadgeModule],
   templateUrl: './recipecard.component.html',
   styleUrl: './recipecard.component.scss'
 })
 export class RecipecardComponent {
+  like : number = 0
+  hidden = false;
+
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
 
 }
