ef74579bfecd98d54f7f5a3ec7571142000c94ef
Nithin1297 <nithin.madduri@proclink.com>
2024-07-26 17:41:16 +0530

    button works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipecardComponent } from './recipecard/recipecard.component';
-import { DashboardComponent } from "./dashboard/dashboard.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RecipecardComponent, DashboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'recipe-app';
-}
+export class AppComponent {}
--- src/app/recipecard/recipecard.component.ts
+++ src/app/recipecard/recipecard.component.ts
@@ -1,41 +1,51 @@
 import { Component, Input } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from '../app.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { RecipeDataService } from '../recipe-data.service';
 
-
 @Component({
   selector: 'app-recipecard',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,AppComponent,MatIconModule,MatBadgeModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    AppComponent,
+    MatIconModule,
+    MatBadgeModule,
+  ],
   templateUrl: './recipecard.component.html',
-  styleUrl: './recipecard.component.scss'
+  styleUrl: './recipecard.component.scss',
 })
 export class RecipecardComponent {
+  constructor(public recepinfo: RecipeDataService) {}
 
-  constructor(public recepinfo : RecipeDataService){
+  @Input() item = {
+    itemTitle: 'Chicken Biriyani',
+    itemposter:
+      'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/4:3/w_5100,h_3825,c_limit/RoastChicken_RECIPE_080420_37993.jpg',
+    itemImage:
+      'https://m.media-amazon.com/images/I/81j-Ml3dQfL._AC_UF1000,1000_QL80_.jpg',
+    itemType: 'Non-Veg',
+    itemDuration: '55 Minnutes',
+    itemLink:
+      'https://www.youtube.com/watch?v=95BCU1n268w&ab_channel=SpiceEats',
+    itemProcedure:
+      'Marinate chicken with yogurt, ginger, garlic, and spices. Cook with ground masala and rice.',
+    itemIngredients:
+      'Chicken, basmati rice, yogurt, ginger, garlic, onions, ghee, cooking oil, spices',
+  };
 
-  }
-  
-  @Input() item =  {
-    itemTitle : "Chicken Biriyani",
-    itemposter :"https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/4:3/w_5100,h_3825,c_limit/RoastChicken_RECIPE_080420_37993.jpg",
-    itemImage : "https://m.media-amazon.com/images/I/81j-Ml3dQfL._AC_UF1000,1000_QL80_.jpg",
-    itemType : "Non-Veg",
-    itemDuration : "55 Minnutes",
-    itemLink : "https://www.youtube.com/watch?v=95BCU1n268w&ab_channel=SpiceEats",
-    itemProcedure :  "Marinate chicken with yogurt, ginger, garlic, and spices. Cook with ground masala and rice.",
-    itemIngredients : "Chicken, basmati rice, yogurt, ginger, garlic, onions, ghee, cooking oil, spices",
-  }
-
-  like : number = 0
   hidden = false;
 
   toggleBadgeVisibility() {
     this.hidden = !this.hidden;
   }
 
+  like = 0;
+  likeIncreament() {
+    this.like += 1;
+  }
 }
