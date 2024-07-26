21f3b34b7bb6bdbafea01703fe042a3493d21e7a
phanidhar1405 <saiphanidhar.guttula@proclink.com>
2024-07-26 16:11:59 +0530

    phani code

---
+++ src/app/recipe-overview/recipe-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-overview.component.html',
+  styleUrl: './recipe-overview.component.scss'
+})
+export class RecipeOverviewComponent {
+
+}
