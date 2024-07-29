a51ac8897b4190b5960aafb199ca9e24b093866c
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-07-29 12:16:20 +0530

    routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { RecipecardComponent } from './recipecard/recipecard.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RecipecardComponent, DashboardComponent],
+  imports: [RouterOutlet, RecipecardComponent, DashboardComponent,LandingPageComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,25 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SearchComponent } from './search/search.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [
-  // {
-  //     path: '',
-  //     component : HomePageComponent
-  // },
-  //   {
-  //     path: 'dashboard',
-  //     component: DashboardComponent,
-  //   },
+
+  export const routes: Routes = [
+    {
+        path: '',
+        component : LandingPageComponent,
+    },
+    {
+        path : 'recipes',
+        component: DashboardComponent,
+    },
+    {
+        path : 'search',
+        component: SearchComponent,
+    },
+    {
+        path : '**',
+        component: PageNotFoundComponent,
+    },
 ];
